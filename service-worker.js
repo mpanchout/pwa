b1669a883f81d2b3717fa03ada6b2b33999556cb
mpanchout <manon.panchout@edu.univ-fcomte.fr>
2023-11-27 11:25:00 +0100

    add service worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = "pwaApp";
+const appShellFiles = [
+    "icons/icon-192.png",
+    "icons/icon-512.png",
+    "icons/maskable_icon.png",
+    "pwa.manifest",
+    "app.js",
+    "index.html",
+    "style.css"
+];
+
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName);
+        console.log("[Service Worker] Caching all: app shell and content");
+        await cache.addAll(contentToCache);
+      })(),
+    );
+  });
